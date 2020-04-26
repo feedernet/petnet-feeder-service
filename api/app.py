0f03b47897d4a2ce91df1a54e8cdd13124721965
ted <ted@timmons.me>
2020-04-25 17:59:26 -0700

    spacing, https, refactor
    - move GET and POST to their own methods
    - use a default route to catch things
    - switch to 2 spaces (which makes diffs hard, add `?w=1` to view)
    - remove inaccessible `return`

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, Response
 from hashlib import sha1
 import json
@@ -9,50 +11,58 @@ devices = {}
 # Create the flask app
 app = Flask(__name__)
 
+# FYI, logging:
+# https://flask.palletsprojects.com/en/1.1.x/logging/#basic-configuration
+
 # Generate "hardware IDs" per device
 def generateHid(uid):
-    return sha1(uid.encode('utf-8')).hexdigest()
+  return sha1(uid.encode('utf-8')).hexdigest()
 
-# Welcome :)
-@app.route('/')
-@app.route('/api')
-@app.route('/api/v1')
+# Welcome :) using this as a catch-all for all the methods we haven't implemented.
+# stolen from here: https://flask.palletsprojects.com/en/1.1.x/patterns/singlepageapplications/
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def welcome():
-    return "🤖😻"
+  return "🤖😻"
 
 # Fetch or create a device
-@app.route('/api/v1/kronos/devices', methods=['GET', 'POST'])
+@app.route('/api/v1/kronos/devices', methods=['POST'])
 def manage_devices():
-    # Handle POST (create)
-    if request.method == 'POST':
-        device = request.json
-        # Ensure all devices have a "uid"
-        if 'uid' not in device:
-            return Response(status=400)
-        # Generate the HID
-        hid = generateHid(device['uid'])
-        # Check if we already have this device
-        if hid in devices:
-            return {
-                "hid" : hid,
-                "links" : {},
-                "message" : "device is already registered"
-            }
-        else:
-            # We don't have this device, so add it
-            devices[hid] = device
-            return {
-                "hid" : hid,
-                "links" : {},
-                "message" : "device was registered successfully"
-            }
-        return Response(status=200)
+  # Handle POST (create)
+  device = request.json
+  # Ensure all devices have a "uid"
+  if 'uid' not in device:
+    app.logger.warning("device didn't contain a UID, returning 400")
+    return Response(status=400)
+  # Generate the HID
+  hid = generateHid(device['uid'])
+  # Check if we already have this device
+  if hid in devices:
+    return {
+      "hid" : hid,
+      "links" : {},
+      "message" : "device is already registered"
+    }
+  # else We don't have this device, so add it
+  devices[hid] = device
+  return {
+    "hid" : hid,
+    "links" : {},
+    "message" : "device was registered successfully"
+  }
 
-    # Handle GET (fetch)
-    elif request.method == 'GET':
-        # For now, allow us to fetch all of the devices
-        return json.dumps(devices)
+
+@app.route('/api/v1/kronos/devices', methods=['GET'])
+def get_devices():
+  # Handle GET (fetch)
+  # For now, allow us to fetch all of the devices
+  return json.dumps(devices)
 
 # Run the app if we're executing this file
 if __name__ == '__main__':
-    app.run()
+  if os.environ.get('SSH_PLZ'):
+    # run with SSL enabled (on the same port); not defaulting because it requires sudo to start.
+    # instaed, could put in haproxy or use iptables: https://superuser.com/a/1334552
+    app.run(ssl_context='adhoc', host='0.0.0.0', port=443)
+  app.run()
+
