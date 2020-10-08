9636336433867065412d3a00e34ee63c64340031
Kenny Root <kenny@the-b.org>
2020-10-07 19:22:21 -0700

    Remove old investigations
    We do not needs these, but it is still in git history.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -exv
-
-BASE=$(dirname $(readlink -f $0))
-# 
-docker run -it -u $(id -u):$(id -g) -v $BASE/conf:/home/mitmproxy/.mitmproxy:z -p 7112:8080 -p 127.0.0.1:8081:8081 -e HOME=/home/mitmproxy mitmproxy/mitmproxy /bin/sh -c /usr/bin/mitmweb --set ssl_insecure=true
-
