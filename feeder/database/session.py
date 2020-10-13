637348bed098411ade1ac24be2f6ee8c19fee888
Kenny Root <kenny@the-b.org>
2020-10-12 21:13:52 -0700

    Enable foreign keys pragma
    By default SQLite does not enable foreign keys, so enable it when
    the connection is checked out of the pool.

--- feeder/database/session.py
+++ feeder/database/session.py
@@ -1,9 +1,14 @@
 from databases import Database
 from sqlalchemy import MetaData
+from sqlalchemy import event
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.pool import Pool
 
 from feeder import settings
 
+@event.listens_for(Pool, 'checkout')
+def _fk_pragma_on_connect(dbapi_con, con_record, con_proxy):
+    dbapi_con.execute('pragma foreign_keys=ON')
 
 db = Database(f"sqlite:///{settings.database_path}")
 
