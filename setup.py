9292a4dda76968a14454cecedb9be57352ac0647
Marc Billow <mbillow@indeed.com>
2020-12-31 16:48:21 -0600

    Setup tox, pytest, flake8, black, and pylint

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='petnet-api-replacement',
     version='0.1dev',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     license='MIT',
     long_description=open('README.md').read(),
     entry_points={
---
+++ tests/test_kronos_api.py
@@ -0,0 +1,2 @@
+def test_kronos_gateway_list():
+    assert 2 + 2 == 4
