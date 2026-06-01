213c4bd4a2238914247eb605d113395105f5a3fd
Marc Billow <mbillow@me.com>
2026-06-01 11:46:16 +0200

    feat: switch to structured JSON logging

--- feeder/config.py
+++ feeder/config.py
@@ -4,13 +4,14 @@ LOGGING_CONFIG = {
     "version": 1,
     "disable_existing_loggers": True,
     "formatters": {
-        "default": {
-            "format": "%(asctime)s   %(levelname)-8s %(name)s: %(message)s",
+        "json": {
+            "()": "pythonjsonlogger.jsonlogger.JsonFormatter",
+            "format": "%(asctime)s %(name)s %(levelname)s %(message)s"
         }
     },
     "handlers": {
         "default": {
-            "formatter": "default",
+            "formatter": "json",
             "class": "logging.StreamHandler",
             "stream": "ext://sys.stderr",
         }
